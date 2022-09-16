4e101762fb3c5418abdf21162292c6336fe45cd5
Shavursky <shavursky@gmail.com>
2022-09-16 23:02:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const div = document.querySelector('.gallery');
 const button = document.querySelector('.load-more');
 let query = '';
 let page = 1;
+let pagesCount = 0;
+let perPage = 40;
 const API_KEY = '29835130-c19dfd8cc48587c57807c0f38';
 let gallery;
 
@@ -15,23 +17,23 @@ formImg.addEventListener('submit', (e) => {
   e.preventDefault();
   clearArticlesContainer();
   query = e.target.searchQuery.value.trim();  
-  onSearch(query, 1, 40).then(()=>{
+  onSearch(query, 1, perPage).then(() => {
     gallery = new SimpleLightbox('.gallery a');  // SimpleLightbox
   });
 });
 
 button.addEventListener('click', () => {  
   page += 1; 
-  onSearch(query, page, 40).then(()=>{
+  onSearch(query, page, perPage).then(() => {
       gallery.refresh();  // simplelightbox   loadMore 
       
       const { height: cardHeight } = document   //скрод (прокручування)
-      .querySelector(".gallery")
-      .firstElementChild.getBoundingClientRect();
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
       window.scrollBy({
         top: cardHeight * 3,
         behavior: "smooth",
-});
+      });
   });
 });
 
@@ -42,16 +44,22 @@ async function fetchImages(images, page, perPage) {
 function onSearch(searchImages, page, perPage) {    
   return fetchImages(searchImages, page, perPage).then((response) => {      
       const images = response.data.hits;
+      pagesCount = Math.ceil(response.data.totalHits / perPage);
+      console.log(response.data);
       if(images.length === 0) {
         button.style.opacity = 0; 
-          Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+        Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
       } else if (query === '') {
         button.style.opacity = 0;
         Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
-      } else {    
-          button.style.opacity = 1;           
-          const listImages = images.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-              return `<div class="photo-card">
+      } else {
+        if(page < pagesCount){
+          button.style.opacity = 1; 
+        } else{
+          button.style.opacity = 0; 
+        }       
+        const listImages = images.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+          return `<div class="photo-card">
               <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy"/>
               <div class="info">
                 <p class="info-item">
@@ -69,7 +77,7 @@ function onSearch(searchImages, page, perPage) {
               </a>
               </div>
             </div>`}).join('');             
-            div.insertAdjacentHTML('beforeend', listImages);    
+        div.insertAdjacentHTML('beforeend', listImages);  
       } 
   }); 
 }
